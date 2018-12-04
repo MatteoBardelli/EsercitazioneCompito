063d54177b4b9e0d2d9c3e3807b59f9bf95beffa
MatteoBardelli <lapomatte@gmail.com>
2018-12-04 09:47:30 +0100

    Aggiuta commenti

--- EsercizioCompito/EsercizioCompito/EsercizioCompito.cpp
+++ EsercizioCompito/EsercizioCompito/EsercizioCompito.cpp
@@ -2,9 +2,9 @@ using namespace std;
 #include <iostream>;
 
 void main() {
-	int count = 0;
-	for (int i = 0; i < 10; i++) {
-		count++;
-		cout << count << endl;
+	int count = 0;   //Creazione variabile contatrice
+	for (int i = 0; i < 10; i++) {    //ciclo
+		count++;                      //incrementazione della variabile contatore
+		cout << "Variabile contatore"<< count << endl;  //stampa della variabile contatore
 	}
 }
